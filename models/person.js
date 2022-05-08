8be97d1f987df350650ca75cd0891b58cd666aba
srtalaie <srt4721@yahoo.com>
2022-05-07 18:07:44 -0700

    3.19 - 3.21

--- models/person.js
+++ models/person.js
@@ -20,8 +20,14 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        minlength: 10,
-        required: true
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{7}/.test(v) || /\d{2}-\d{8}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number! Must be in either ##-######## or ###-########## formats`
+        }
     }
 })
 
